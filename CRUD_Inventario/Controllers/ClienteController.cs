b0c536ac386f0eb626c2e9d8483e7c8462b2baa1
Leonardo-Moreno-2020 <therealdeadman2012@gmail.com>
2021-06-27 08:53:50 -0500

    creando usuario crud

---
+++ CRUD_Inventario/Controllers/ClienteController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_Inventario.Models;
+
+namespace CRUD_Inventario.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var Data_B = new inventario2021Entities())
+            {
+                return View(Data_B.cliente.ToList());
+            }
+
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(cliente Cliente)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var Data_B = new inventario2021Entities())
+                {
+                    Data_B.cliente.Add(Cliente);
+                    Data_B.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception Cap_error)
+            {
+                ModelState.AddModelError("", "Error" + Cap_error);
+                return View();
+            }
+        }
+        public ActionResult Details(int id)
+        {
+            using (var Data_B = new inventario2021Entities())
+            {
+                cliente Cliente = Data_B.cliente.Find(id);
+                return View(Cliente);
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var Data_B = new inventario2021Entities())
+                {
+                    cliente Cliente = Data_B.cliente.Where(a => a.id == id).FirstOrDefault();
+                    return View(Cliente);
+                }
+            }
+            catch (Exception Cap_error)
+            {
+                ModelState.AddModelError("", "error" + Cap_error);
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cliente clienteEdit)
+        {
+            try
+            {
+                using (var Data_B = new inventario2021Entities())
+                {
+                    var Cliente = Data_B.cliente.Find(clienteEdit.id);
+                    Cliente.nombre = clienteEdit.nombre;
+                    Cliente.documento = clienteEdit.documento;
+                    Cliente.id = clienteEdit.id;
+                    Cliente.email = clienteEdit.email;
+                    Data_B.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+            }
+            catch (Exception Cap_error)
+            {
+                ModelState.AddModelError("", "error" + Cap_error);
+                return View();
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            using (var Data_B = new inventario2021Entities())
+            {
+                var clienteDel = Data_B.cliente.Find(id);
+                Data_B.cliente.Remove(clienteDel);
+                Data_B.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
