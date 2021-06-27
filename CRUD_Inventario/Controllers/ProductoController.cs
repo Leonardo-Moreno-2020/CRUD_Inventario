ba338528ba91e4edeb9bbda359692f41b9106b5d
Leonardo-Moreno-2020 <therealdeadman2012@gmail.com>
2021-06-27 10:26:13 -0500

    producto

---
+++ CRUD_Inventario/Controllers/ProductoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_Inventario.Models;
+
+namespace CRUD_Inventario.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            using (var Data_B = new inventario2021Entities())
+            {
+                return View(Data_B.producto.ToList());
+            }
+            
+        }
+        public static string NombreProveedor(int idProveedor)
+        {
+            using (var Data_B = new inventario2021Entities())
+            {
+                return Data_B.proveedor.Find(idProveedor).nombre;
+            }
+        }
+
+    }
+}
