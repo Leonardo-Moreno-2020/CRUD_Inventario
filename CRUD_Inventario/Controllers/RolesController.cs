232f66eb9820ccecb758f73833079a85e6dd9d55
Leonardo-Moreno-2020 <therealdeadman2012@gmail.com>
2021-07-01 23:24:08 -0500

    upload y otros cambios

--- CRUD_Inventario/Controllers/RolesController.cs
+++ CRUD_Inventario/Controllers/RolesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -100,5 +101,69 @@ namespace CRUD_Inventario.Controllers
 
             }
         }
+        public ActionResult uploadCSV()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult uploadCSV(HttpPostedFileBase fileForm)
+        {
+            //string para guardar la ruta
+            string filePath = string.Empty;
+
+
+            //condicion para saber si llego el archivo
+            if (fileForm != null)
+            {
+                //ruta de la carpeta que gurdara el archivo
+                string path = Server.MapPath("~/Uploads/");
+
+                //condicion para saber si la ruta de la carpeta existe
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                //obtener el nombre del archivo
+                filePath = path + Path.GetFileName(fileForm.FileName);
+                //obtener la extension del archivo
+                string extension = Path.GetExtension(fileForm.FileName);
+
+                //guardar el archivo
+                fileForm.SaveAs(filePath);
+
+                string csvData = System.IO.File.ReadAllText(filePath);
+
+                foreach (string row in csvData.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        var newRoles = new roles
+                        {
+                            descripcion = row.Split(';')[0],
+                          
+                        };
+
+                        using (var Data_B = new inventario2021Entities())
+                        {
+                            Data_B.roles.Add(newRoles);
+                            Data_B.SaveChanges();
+                        }
+                    }
+                }
+
+
+            }
+            return View();
+
+        }
+
+
+
+
     }
+    
+
+
+
 }
